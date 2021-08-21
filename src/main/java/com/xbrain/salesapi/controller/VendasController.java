c4acfa18ebe2985ee4ef226c4c6e7577c152e011
Alexandre Tanaka <alexandre.k.tanaka@gmail.com>
2021-08-21 17:47:49 -0300

    Método para criação de nova venda movido para classe VendedoresController

--- src/main/java/com/xbrain/salesapi/controller/VendasController.java
+++ src/main/java/com/xbrain/salesapi/controller/VendasController.java
@@ -3,7 +3,6 @@ package com.xbrain.salesapi.controller;
 import com.xbrain.salesapi.model.Venda;
 import com.xbrain.salesapi.repository.VendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +17,5 @@ public class VendasController {
     @GetMapping
     public List<Venda> listar() {
         return vendaRepository.findAll();
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Venda novaVenda(@RequestBody Venda venda) {
-        return vendaRepository.save(venda);
     }
 }
