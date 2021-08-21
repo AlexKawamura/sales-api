ba524dcf4d00b2d71ccea19f06db32bbc4047c33
Alexandre Tanaka <alexandre.k.tanaka@gmail.com>
2021-08-21 14:37:31 -0300

    Revert "Criando repository para vendedor"
    This reverts commit 90cbc326375ec2fdfc860dab522cb669834555f9.

--- src/main/java/com/xbrain/salesapi/repository/VendedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xbrain.salesapi.repository;
-
-import com.xbrain.salesapi.model.Vendedor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendedorRepository extends JpaRepository<Vendedor, Long> { }
