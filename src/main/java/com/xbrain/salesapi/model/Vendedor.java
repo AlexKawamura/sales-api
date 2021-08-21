6c4240fee29b2917fd4fa637dd047c2e1f87be7e
Alexandre Tanaka <alexandre.k.tanaka@gmail.com>
2021-08-21 14:36:34 -0300

    Adicionando tags para criação da tabela Vendedor

--- src/main/java/com/xbrain/salesapi/model/Vendedor.java
+++ src/main/java/com/xbrain/salesapi/model/Vendedor.java
@@ -1,6 +1,17 @@
 package com.xbrain.salesapi.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Vendedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String nome;
+    @Column
+    private int total_vendas;
 }
