90cbc326375ec2fdfc860dab522cb669834555f9
Alexandre Tanaka <alexandre.k.tanaka@gmail.com>
2021-08-21 14:37:16 -0300

    Criando repository para vendedor

---
+++ src/main/java/com/xbrain/salesapi/controller/VendedorController.java
@@ -0,0 +1,24 @@
+package com.xbrain.salesapi.controller;
+
+import com.xbrain.salesapi.model.Vendedor;
+
+import com.xbrain.salesapi.repository.VendedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vendedores")
+public class VendedorController {
+
+    @Autowired
+    private VendedorRepository vendedorRepository;
+
+    @GetMapping
+    public List<Vendedor> listar() {
+        return vendedorRepository.findAll();
+    }
+}
