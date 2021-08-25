5b4feba9114eedf21a7f1278bb8ce9f54b43af18
Alexandre Tanaka <alexandre.k.tanaka@gmail.com>
2021-08-25 15:52:23 -0300

    Requisição para retornar uma lista de vendedores com média diária de vendas, conforme período passado por parâmetro

--- src/main/java/com/xbrain/salesapi/controller/VendedoresController.java
+++ src/main/java/com/xbrain/salesapi/controller/VendedoresController.java
@@ -1,7 +1,10 @@
 package com.xbrain.salesapi.controller;
 
 import com.xbrain.salesapi.model.Vendedor;
+import com.xbrain.salesapi.repository.VendaRepository;
 import com.xbrain.salesapi.repository.VendedorRepository;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +18,33 @@ public class VendedoresController {
     @Autowired
     private VendedorRepository vendedorRepository;
 
+    @Autowired
+    private VendaRepository vendaRepository;
+
     @GetMapping
     public List<Vendedor> listar() {
         return vendedorRepository.findAll();
+    }
+
+    @GetMapping("/listar")
+    public List<Vendedor> listarPorPeriodo(@RequestParam String dataInicial, @RequestParam String dataFinal) {
+        DateTime dInicial = new DateTime(dataInicial);
+        DateTime dFinal = new DateTime(dataFinal);
+        int qntDias = Days.daysBetween(dInicial, dFinal).getDays();
+
+        List<Vendedor> vendedorList = vendedorRepository.findAll();
+
+        for (Vendedor vendedor : vendedorList) {
+            int qntVendas = vendaRepository.countByVendedorIdAndDataVendaBetween(vendedor.getId(), dataInicial, dataFinal);
+
+            float mediaVendasDiarias = (float) qntVendas / qntDias;
+
+            vendedor.setMediaVendasDiarias(mediaVendasDiarias);
+
+            vendedorRepository.save(vendedor);
+        }
+
+        return vendedorList;
     }
 
     @PostMapping
